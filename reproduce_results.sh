0b85934f90a370ad6f8767d6c48db008c474ebd2
yuansheng <48724890+Hua-YS@users.noreply.github.com>
2021-08-24 20:40:18 +0200

    Update reproduce_results.sh

--- reproduce_results.sh
+++ reproduce_results.sh
@@ -1,22 +1,22 @@
-#python main.py --dataset multiscene-clean --model_name vggnet16 --weight_path  weights/multiscene-clean/vgg16.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name vggnet19 --weight_path weights/multiscene-clean/vgg19.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name inceptionv3 --weight_path weights/multiscene-clean/inceptionv3.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name resnet50 --weight_path weights/multiscene-clean/res50.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name resnet101 --weight_path weights/multiscene-clean/res101.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name resnet152 --weight_path weights/multiscene-clean/res152.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name squeezenet --weight_path weights/multiscene-clean/squeeze10.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name mobilenetv2 --weight_path weights/multiscene-clean/mobilev2.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name shufflenetv2 --weight_path weights/multiscene-clean/shufflev2.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name densenet121 --weight_path weights/multiscene-clean/dense121.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name densenet169 --weight_path weights/multiscene-clean/dense169.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name resnext50 --weight_path weights/multiscene-clean/resnext50.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name resnext101 --weight_path weights/multiscene-clean/resnext101.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name mnasnet --weight_path weights/multiscene-clean/nas10.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name kfb --weight_path weights/multiscene-clean/kfb.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name facnn --weight_path weights/multiscene-clean/facnn.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name saff --weight_path weights/multiscene-clean/saff.pth.tar --evaluate
-#python main.py --dataset multiscene-clean --model_name lr-vggnet16 --weight_path weights/multiscene-clean/lr_vgg16.pth.tar --nb_moda 32 --nb_units 64 --evaluate
-#python main.py --dataset multiscene-clean --model_name lr-resnet50 --weight_path weights/multiscene-clean/lr_res50.pth.tar --nb_moda 32 --nb_units 64 --evaluate
+python main.py --dataset multiscene-clean --model_name vggnet16 --weight_path  weights/multiscene-clean/vgg16.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name vggnet19 --weight_path weights/multiscene-clean/vgg19.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name inceptionv3 --weight_path weights/multiscene-clean/inceptionv3.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name resnet50 --weight_path weights/multiscene-clean/res50.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name resnet101 --weight_path weights/multiscene-clean/res101.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name resnet152 --weight_path weights/multiscene-clean/res152.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name squeezenet --weight_path weights/multiscene-clean/squeeze10.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name mobilenetv2 --weight_path weights/multiscene-clean/mobilev2.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name shufflenetv2 --weight_path weights/multiscene-clean/shufflev2.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name densenet121 --weight_path weights/multiscene-clean/dense121.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name densenet169 --weight_path weights/multiscene-clean/dense169.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name resnext50 --weight_path weights/multiscene-clean/resnext50.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name resnext101 --weight_path weights/multiscene-clean/resnext101.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name mnasnet --weight_path weights/multiscene-clean/nas10.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name kfb --weight_path weights/multiscene-clean/kfb.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name facnn --weight_path weights/multiscene-clean/facnn.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name saff --weight_path weights/multiscene-clean/saff.pth.tar --evaluate
+python main.py --dataset multiscene-clean --model_name lr-vggnet16 --weight_path weights/multiscene-clean/lr_vgg16.pth.tar --nb_moda 32 --nb_units 64 --evaluate
+python main.py --dataset multiscene-clean --model_name lr-resnet50 --weight_path weights/multiscene-clean/lr_res50.pth.tar --nb_moda 32 --nb_units 64 --evaluate
 
 python main.py --dataset multiscene --model_name vggnet16 --weight_path  weights/multiscene/vgg16.pth.tar --evaluate
 python main.py --dataset multiscene --model_name vggnet19 --weight_path weights/multiscene/vgg19.pth.tar --evaluate
